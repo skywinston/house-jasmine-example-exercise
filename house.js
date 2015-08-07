1f7c60de497d43cce26a25c5195bdd24716009d8
Sky Winston <skywinston@gmail.com>
2015-08-07 13:24:49 -0600

    Refactored solution to be more concise

--- house.js
+++ house.js
@@ -2,15 +2,22 @@ function House (numberOfDoors, numberOfWindows) {
   this.numberOfDoors = numberOfDoors;
   this.numberOfWindows = numberOfWindows;
   this.toString = function(){
-    if(this.numberOfDoors > 1 && this.numberOfWindows === 1){
-      return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " window";
-    } else if ((this.numberOfDoors>1 && this.numberOfWindows>1) || (this.numberOfDoors<1 && this.numberOfWindows<1)){
-      return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " windows";
-    } else if (this.numberOfDoors === 1 && this.numberOfWindows>1){
-      return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " windows";
-    } else if (this.numberOfDoors===1 && this.numberOfWindows===1){
-      return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " window";
-    }
+    // This is a much cleaner, and better solution, via Brett:
+    var houseString = (this.numberOfDoors != 1) ? "doors" : "door";
+    var windowString = (this.numberOfWindows != 1) ? "windows" : "window";
+
+    return "The house has " + this.numberOfDoors + " " + houseString + " and " + this.numberOfWindows + " " + windowString;
+
+    // My original and really verbose solution:
+    // if(this.numberOfDoors > 1 && this.numberOfWindows === 1){
+    //   return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " window";
+    // } else if ((this.numberOfDoors>1 && this.numberOfWindows>1) || (this.numberOfDoors<1 && this.numberOfWindows<1)){
+    //   return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " windows";
+    // } else if (this.numberOfDoors === 1 && this.numberOfWindows>1){
+    //   return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " windows";
+    // } else if (this.numberOfDoors===1 && this.numberOfWindows===1){
+    //   return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " window";
+    // }
   }
 }
 
