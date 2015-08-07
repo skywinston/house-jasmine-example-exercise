4982892a84d6d3857c2df06189fd2930db9116f4
Sky Winston <skywinston@gmail.com>
2015-08-07 10:56:38 -0600

    wrote and passed first test for House

---
+++ house.js
@@ -0,0 +1,5 @@
+function House (numberOfDoors) {
+  this.numberOfDoors = numberOfDoors;
+}
+
+module.exports = House; // This is a command specific to node
---
+++ spec/house_spec.js
@@ -0,0 +1,11 @@
+var House = require('../house.js');
+
+  describe('House', function () {
+    describe('constructor', function () {
+      it('uses the numberOfDoors argument as a property', function () {
+        var house = new House(100);
+
+        expect(house.numberOfDoors).toEqual(100);
+      });
+    });
+  });
